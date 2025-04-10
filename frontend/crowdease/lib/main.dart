7b5734406f40b1903f4fd21ae28232c6dd9aa971
HNavanjani <hnavanjani11@gmail.com>
2025-04-10 13:11:25 +1000

    Configured Firebase using FlutterFire CLI and generated firebase_options.dart for Android and iOS

--- frontend/crowdease/lib/main.dart
+++ frontend/crowdease/lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; 
+
 import 'features/common/screens/splash_screen.dart';
 import 'features/common/screens/home_screen.dart';
+import 'features/common/screens/login_screen.dart'; 
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CrowdEase',
       theme: ThemeData(primarySwatch: Colors.indigo),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: const SplashScreen(), 
       routes: {
-        '/home': (context) => const HomeScreen(), // 👈 This is the fix
+        '/home': (context) => const HomeScreen(),
+        '/login': (context) => const LoginScreen(), 
       },
     );
   }
