62f4e2b3ca632174f0e6fc1536c06f4a3af615e3
KISALKA\HNavanjani <hnavanjani11@gmail.com>
2025-04-21 23:09:45 +1000

    Temporarily switch API base URL to local backend for testing

--- frontend/crowdease/lib/config.dart
+++ frontend/crowdease/lib/config.dart
@@ -1,5 +1,9 @@
 const bool isLocal = true; // false if deployed
 
-final String baseUrl = isLocal
-    ? 'http://10.0.2.2:8000'  // For emulator to access local backend
-    : 'https://crowdease-api.onrender.com'; // Render backend URL
+// final String baseUrl = isLocal
+//     // ? 'http://10.0.2.2:8000'  // For emulator to access local backend
+//     ? 'https://crowdease-api.onrender.com'  // For emulator to access local backend
+//     : 'https://crowdease-api.onrender.com'; // Render backend URL
+
+final String baseUrl = 'http://10.0.2.2:8000';
+
--- frontend/crowdease/lib/core/api_constants.dart
+++ frontend/crowdease/lib/core/api_constants.dart
@@ -1,3 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'https://crowdease-api.onrender.com';
+  // static const String baseUrl = 'https://crowdease-api.onrender.com';
+  static const String baseUrl = 'http://10.0.2.2:8000';
+
 }
